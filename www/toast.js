83bd0da6f9172b8e9cf6a81fca7986939b9a4881
navy <navyxie2010@gmail.com>
2014-05-28 12:03:40 +0800

    modify classname

--- www/toast.js
+++ www/toast.js
@@ -1,6 +1,6 @@
 var toast = {
     ShowToast:function(content,length){
-        cordova.exec(null, null,"Toast","toast",[content,length]);
+        cordova.exec(null, null,"ToastPlugin","toast",[content,length]);
     }
 }
 module.exports = toast;
